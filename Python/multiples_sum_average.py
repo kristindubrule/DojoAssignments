6e36cc2b2b4b06ab5dd60024d5a2f8f11c63e42f
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-05 11:46:38 -0800

    Initial checkin

---
+++ Python/multiples_sum_average.py
@@ -0,0 +1,20 @@
+for x in range(1,1001):
+	if x%2 == 0:
+		print x
+
+for x in range(5,1000001):
+	if x%5 == 0:
+		print x
+
+
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for element in a:
+	sum += element
+print sum
+
+sum = 0
+for element in a:
+	sum += element
+print sum/len(a)
+
