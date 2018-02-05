5e5d5687d444aa4b6fef5159919b5ad38b639840
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-05 15:38:48 -0800

    Adding support for strings

--- Python/stars.py
+++ Python/stars.py
@@ -1,8 +1,13 @@
 def draw_stars(col_arr):
 	for cols in col_arr:
-		for i in range (0,cols):
-			print "*",
+		if isinstance(cols,int):
+			for i in range (0,cols):
+				print "*",
+		elif isinstance(cols,str):
+			for i in range (0,len(cols)):
+				print cols[0].lower(),
 		print
 
 draw_stars([4,6,1,3,5,7,25])
 draw_stars([])
+draw_stars([4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"])
