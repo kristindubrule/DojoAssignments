1ae004f86776436ae26a27d4cdf10e87d2abe1d9
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-05 16:01:14 -0800

    Initial checkin

---
+++ Python/names.py
@@ -0,0 +1,32 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def printNames(list1):
+	for idx, item in enumerate(list1):
+		name = (item["first_name"] + " " + item["last_name"]).upper()
+		print idx+1, " - ", name, " - ", len(name)-1
+
+def printStudentsInstructors(list1):
+	for key, child_list in list1.iteritems():
+		print key
+		printNames(child_list)
+
+printNames(students)
+printStudentsInstructors(users)
