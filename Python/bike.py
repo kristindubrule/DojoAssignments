08203cfaeb8f93b4264cc1ca6f91f5c7c0e322e5
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-06 13:08:51 -0800

    Initial checkin

---
+++ Python/bike.py
@@ -0,0 +1,33 @@
+class Bike(object):
+    def __init__(self, price, max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+    def displayInfo(self):
+        print "Bike price ${}, maximum speed: {}, current miles {}.".format(self.price, self.max_speed, self.miles)
+        return self
+    def ride(self):
+        print "Riding"
+        self.miles += 10
+        return self
+    def reverse(self):
+        print "Reversing"
+        self.miles -= 5
+        return self
+
+bike1 = Bike(500, 20)
+for i in range(3):
+    bike1.ride()
+bike1.reverse()
+bike1.displayInfo()
+
+bike2 = Bike(2000, 30)
+for i in range(2):
+    bike2.ride()
+    bike2.reverse()
+bike2.displayInfo()
+
+bike3 = Bike(10000,50)
+for i in range(3):
+    bike3.reverse()
+bike3.displayInfo()
