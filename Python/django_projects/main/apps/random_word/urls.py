b994c1b74808f850f16171c615973e31cd57b966
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-15 15:00:04 -0800

    Initial checkin

---
+++ Python/django_projects/main/apps/random_word/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+
+urlpatterns = [
+	url(r'^$', views.index),
+	url(r'generate$', views.generate),
+	url(r'reset$', views.reset)
+]
---
+++ Python/django_projects/main/apps/random_word/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
+# Create your views here.
+def index(request, random_word=""):
+	context = {
+		"word": get_random_string(14),
+	}
+	if 'attempt_count' in request.session:
+		request.session['attempt_count'] += 1
+	else:
+		request.session['attempt_count'] = 1
+	return render(request, "random_word/index.html", context)
+
+def generate(request):
+	return redirect('/random_word')
+
+def reset(request):
+	del request.session['attempt_count']
+	return redirect('/random_word')
