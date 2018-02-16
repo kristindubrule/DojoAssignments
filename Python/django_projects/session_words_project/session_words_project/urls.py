37cc89d3d8d251dc55c0bb4d08d6b413760dfb9a
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-16 15:08:40 -0800

    Simplified urls to take advantage of single app project

--- Python/django_projects/session_words_project/session_words_project/urls.py
+++ Python/django_projects/session_words_project/session_words_project/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^session_words_app', include('apps.session_words_app.urls')),
+    url(r'^', include('apps.session_words_app.urls')),
 ]
