5c5cc1a886d6f7f884189a7b46d08ad81f0b96dc
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-19 13:57:30 -0800

    Initial checkin

---
+++ Python/django_projects/likes_books/apps/likes_books_app/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class LikesBooksAppConfig(AppConfig):
+    name = 'likes_books_app'
---
+++ Python/django_projects/likes_books/apps/likes_books_app/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class User(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	email = models.EmailField()
+class Book(models.Model):
+	name = models.CharField(max_length=255)
+	desc = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	uploader = models.ForeignKey(User, related_name="uploaded_books")
+	liked_users = models.ManyToManyField(User, related_name="liked_books")
