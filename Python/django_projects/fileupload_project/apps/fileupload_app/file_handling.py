54cfbf8753163e120f0c0b4cdcd633cfb4f2d6b4
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-02-19 09:16:08 -0800

    Changed to actually use django's form generation

--- Python/django_projects/fileupload_project/apps/fileupload_app/file_handling.py
+++ Python/django_projects/fileupload_project/apps/fileupload_app/file_handling.py
@@ -1,4 +1,4 @@
 def handle_uploaded_file(f):
-	with open('/Users/kristinf/Desktop/DojoAssignments/uploaded_files/test.txt', 'wb+') as destination:
+	with open('/Users/kristinf/Desktop/DojoAssignments/uploaded_files/'+f.name, 'wb+') as destination:
 		for chunk in f.chunks():
 			destination.write(chunk)
--- Python/django_projects/fileupload_project/apps/fileupload_app/views.py
+++ Python/django_projects/fileupload_project/apps/fileupload_app/views.py
@@ -6,7 +6,8 @@ from .forms import UploadFileForm
 from .file_handling import handle_uploaded_file
 
 def index(request):
-	return render(request, "fileupload_app/index.html")
+	form = UploadFileForm()
+	return render(request, 'fileupload_app/index.html', {'form': form})
 
 def new(request):
 	response = "Hello, I am a form to create something new!"
@@ -15,14 +16,11 @@ def new(request):
 def upload_file(request):
 	if request.method == 'POST':
 		form = UploadFileForm(request.POST, request.FILES)
-		print "Posting", form
 		if form.is_valid():
 			print "Form is valid"
 			handle_uploaded_file(request.FILES['file'])
 			return HttpResponseRedirect('/success/url/')
-	else:
-		form = UploadFileForm()
-	return render(request, 'fileupload_app/index.html', {'form': form})
+	return redirect('/')
 
 def success(request):
 	return render(request, "fileupload_app/upload.html")
