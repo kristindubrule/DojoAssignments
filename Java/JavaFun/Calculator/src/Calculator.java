8799fed687bc502dd55754f52ff6afd740e23c9f
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-03-06 17:34:12 -0800

    Added support for /, *

--- Java/JavaFun/Calculator/src/Calculator.java
+++ Java/JavaFun/Calculator/src/Calculator.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.io.*;
+
 public class Calculator implements java.io.Serializable {
 
     private double operandOne;
     private double operandTwo;
     private double result;
     private char operation;
+    private ArrayList<Object> components = new ArrayList<Object>();
 
     public Calculator() {
     }
@@ -40,6 +44,27 @@ public class Calculator implements java.io.Serializable {
         this.operation = operation;
     }
 
+    public ArrayList<Object> getComponents() {
+        return components;
+    }
+
+    public void setComponents(ArrayList<Object> components) {
+        this.components = components;
+    }
+
+    public void addComponent(double number) {
+        components.add(number);
+    }
+
+    public void addComponent(String component) {
+        if (component.equals("=")) {
+            result = evaluateExpression(0,components.size()-1);
+            components.clear();
+        } else {
+            components.add(component);
+        }
+    }
+
     public void performOperation() {
         if (operation == '-') {
             performSubtraction();
@@ -56,4 +81,41 @@ public class Calculator implements java.io.Serializable {
         result = operandOne + operandTwo;
     }
 
+    private int indexSubarray(int start, int end, String search) {
+        for (int i = end; i >= start; i--) {
+            String component = components.get(i).toString();
+            if (component == search) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private double evaluateExpression(int start, int end) {
+        if (end == start) {
+            String component = components.get(start).toString();
+            return Double.parseDouble(component.replaceAll("\\s",""));
+        } else {
+            int index = indexSubarray(start, end, "+");
+            if (index >= 0) {
+                return evaluateExpression(start, index - 1) + evaluateExpression(index + 1, end);
+            }
+
+            index = indexSubarray(start, end, "-");
+            if (index >= 0) {
+                return evaluateExpression(start, index - 1) - evaluateExpression(index + 1, end);
+            }
+
+            index = indexSubarray(start, end, "*");
+            if (index >= 0) {
+                return evaluateExpression(start, index - 1) * evaluateExpression(index + 1, end);
+            }
+
+            index = indexSubarray(start, end, "/");
+            if (index >= 0) {
+                return evaluateExpression(start, index - 1) / evaluateExpression(index + 1, end);
+            }
+        }
+        return 0;
+    }
 }
--- Java/JavaFun/Calculator/src/CalculatorTest.java
+++ Java/JavaFun/Calculator/src/CalculatorTest.java
@@ -13,5 +13,28 @@ public class CalculatorTest {
         c.setOperandTwo(3);
         c.performOperation();
         System.out.println(c.getResult());
+
+        Calculator c2 = new Calculator();
+        c2.addComponent(10.5);
+        c2.addComponent("+");
+        c2.addComponent(5.2);
+        c2.addComponent("=");
+        System.out.println(c2.getResult());
+
+        c2.addComponent(10.5);
+        c2.addComponent("+");
+        c2.addComponent(5.2);
+        c2.addComponent("*");
+        c2.addComponent(10);
+        c2.addComponent("=");
+        System.out.println(c2.getResult());
+
+        c2.addComponent(10.5);
+        c2.addComponent("+");
+        c2.addComponent(5.2);
+        c2.addComponent("*");
+        c2.addComponent(-10);
+        c2.addComponent("=");
+        System.out.println(c2.getResult());
     }
 }
