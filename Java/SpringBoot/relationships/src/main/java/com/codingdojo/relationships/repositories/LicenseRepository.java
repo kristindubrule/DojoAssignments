368edad0348ff91cb5af699eb7aad3d95539a451
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-03-15 12:04:47 -0700

    Added repository annotation, fixed hibernate dialect; changed root password to root

--- Java/SpringBoot/relationships/src/main/java/com/codingdojo/relationships/repositories/LicenseRepository.java
+++ Java/SpringBoot/relationships/src/main/java/com/codingdojo/relationships/repositories/LicenseRepository.java
@@ -2,7 +2,9 @@ package com.codingdojo.relationships.repositories;
 
 import com.codingdojo.relationships.models.License;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LicenseRepository extends CrudRepository<License, Long> {
 
 }
