9064a259b273c5e5322674ac373c8e575a064c2a
Kristin Dubrule <kristin.dubrule@gmail.com>
2018-04-04 13:23:15 -0700

    Refactor & add example html file

--- MEAN/node/content_module/static.js
+++ MEAN/node/content_module/static.js
@@ -1,29 +1,31 @@
 const fs = require("fs");
 
 module.exports = function (request,response) {
-	if (request.url.endsWith("html")) {
-		fs.readFile("./views/"+request.url, 'utf8', function (errors, contents) {
-			response.writeHead(200, {'Content-type': 'text/html'});
-	      	response.write(contents); 
-	      	response.end();
-      });
-	} else if (request.url.endsWith("css")) {
-		console.log(request.url);
-		fs.readFile("."+request.url, 'utf8', function (errors, contents) {
-			response.writeHead(200, {'Content-Type': 'text/css'});
-	      	response.write(contents);
-	      	response.end();
-    	});
-	} else if (request.url.endsWith("css")) {
-		console.log(request.url);
-		fs.readFile("."+request.url, 'utf8', function (errors, contents) {
-			response.writeHead(200, {'Content-Type': 'text/css'});
-	      	response.write(contents);
-	      	response.end();
-    	});
-	} else {
-		response.write("not recognized");
-		response.end();
-	}
+	 if (request.url.endsWith("png")) {
+ 		fs.readFile("."+request.url, function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/png'});
+            response.write(contents);
+            response.end();
+        });
+ 	} else {
+		let content_type;
+		let path = "."+request.url;
+	 	if (request.url.endsWith("html") || request.url === '/') {
+			content_type = "text/html";
+			path = "./views/";
+			if (request.url == '/') {
+				path += "index.html";
+			} else {
+				path += request.url;
+			}
+		} else if (request.url.endsWith("css")) {
+			content_type = "text/css";
+		}
+		fs.readFile(path, 'utf8', function (errors, contents) {
+			response.writeHead(200, {'Content-type': content_type});
+		   	response.write(contents); 
+		   	response.end();
+	    });
+	} 
 	return;
 }
